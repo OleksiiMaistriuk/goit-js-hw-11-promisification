b334741ce6e1fcee009e38d1ef1bf1e1ddc49a60
OleksiiMaistriuk <65893898+OleksiiMaistriuk@users.noreply.github.com>
2020-12-20 10:08:38 +0100

    сделал 2 дз

--- src/index.js
+++ src/index.js
@@ -10,39 +10,56 @@
 // delay(1000).then(logger);
 // delay(1500).then(logger);
 
-const users = [
-  { name: 'Mango', active: true },
-  { name: 'Poly', active: false },
-  { name: 'Ajax', active: true },
-  { name: 'Lux', active: false },
-];
+// const users = [
+//   { name: 'Mango', active: true },
+//   { name: 'Poly', active: false },
+//   { name: 'Ajax', active: true },
+//   { name: 'Lux', active: false },
+// ];
 
-const toggleUserState = (allUsers, userName, callback) => {
-  const updatedUsers = allUsers.map(user =>
-    user.name === userName ? { ...user, active: !user.active } : user,
-  );
-
-  callback(updatedUsers);
-};
-
-const logger = updatedUsers => console.table(updatedUsers);
-
-/*
- * Сейчас работает так
- */
-toggleUserState(users, 'Mango', logger);
-toggleUserState(users, 'Lux', logger);
-
-/*
- * Должно работать так
- */
-toggleUserState(users, 'Mango').then(logger);
-toggleUserState(users, 'Lux').then(logger);
-
-// const randomIntegerFromInterval = (min, max) => {
-//   return Math.floor(Math.random() * (max - min + 1) + min);
+// const toggleUserState = (allUsers, userName) => {
+//   return new Promise(resolve => {
+//     const updatedUsers = allUsers.map(user =>
+//       user.name === userName ? { ...user, active: !user.active } : user,
+//     );
+//     resolve(updatedUsers);
+//   });
 // };
 
+// const logger = updatedUsers => console.table(updatedUsers);
+
+// toggleUserState(users, 'Mango').then(logger);
+// toggleUserState(users, 'Lux').then(logger);
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = (transaction, onSuccess, onError) => {
+  const delay = randomIntegerFromInterval(200, 500);
+
+  setTimeout(() => {
+    const canProcess = Math.random() > 0.3;
+
+    if (canProcess) {
+      onSuccess(transaction.id, delay);
+    } else {
+      onError(transaction.id);
+    }
+  }, delay);
+};
+
+const logSuccess = (id, time) => {
+  console.log(`Transaction ${id} processed in ${time}ms`);
+};
+
+const logError = id => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
+
+/*
+//  * Работает так
+//  */
 // const makeTransaction = (transaction, onSuccess, onError) => {
 //   const delay = randomIntegerFromInterval(200, 500);
 
@@ -56,18 +73,6 @@ toggleUserState(users, 'Lux').then(logger);
 //     }
 //   }, delay);
 // };
-
-// const logSuccess = (id, time) => {
-//   console.log(`Transaction ${id} processed in ${time}ms`);
-// };
-
-// const logError = id => {
-//   console.warn(`Error processing transaction ${id}. Please try again later.`);
-// };
-
-// /*
-//  * Работает так
-//  */
 // makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
 // makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
 // makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
